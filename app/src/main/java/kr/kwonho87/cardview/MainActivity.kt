3e2db8a16f96bb38722f052f769f5f2c59f9cca2
1002395 <kwonho87@sk.com>
2019-03-11 14:30:40 +0900

    init

---
+++ app/src/main/java/kr/kwonho87/cardview/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.kwonho87.cardview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
