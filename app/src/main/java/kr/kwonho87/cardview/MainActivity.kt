1f4936354e8b2fc8caf537be5a5a543d977568e7
kwonho87 <kwonho87@gmail.com>
2019-03-18 16:42:18 +0900

    * working...

--- app/src/main/java/kr/kwonho87/cardview/MainActivity.kt
+++ app/src/main/java/kr/kwonho87/cardview/MainActivity.kt
@@ -27,27 +27,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var values = ArrayList<String>()
-//        values[0] = "0"
-//        values[1] = "1"
-//        values[2] = "2"
-//        values[3] = "3"
-//        values[4] = "4"
-//        values[5] = "5"
-//        values[6] = "6"
-//        values[7] = "7"
-//        values[8] = "8"
-//        values[9] = "9"
-
-//        values.add("0")
-//        values.add("1")
-//        values.add("2")
-//        values.add("3")
-//        values.add("4")
-//        values.add("5")
-//        values.add("6")
-//        values.add("7")
-//        values.add("8")
-
         values.add("A")
         values.add("B")
         values.add("C")
